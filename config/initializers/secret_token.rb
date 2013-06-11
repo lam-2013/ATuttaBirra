e7fbf0706c6c4b61760b667c38a78cf38373a9f7
NoemiBodino <baudi90@libero.it>
2013-06-11 14:26:39 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ATuttaBirra::Application.config.secret_token = '495424017712bc6ca30397d49cde22fa2be5ea1169a577c3b15adf398d7f7a663cd78fab74d5905fa4056299493498e5ecc0f414e8473226fb02cfb281c4628a'
